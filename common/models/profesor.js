fd16894d0f1ca96884fdd9e2d4b8017600d090fd
Alberto Sierra <alberto.sierra@murciaeduca.es>
2019-03-27 14:15:38 +0100

    Hasta los hook remotos

--- common/models/profesor.js
+++ common/models/profesor.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Profesor) {
-
+  Profesor.beforeRemote('prototype.__create__visitas', function(context, instance, next) {
+    var fechaManyana = new Date();
+    fechaManyana.setDate(fechaManyana.getDate() + 10);
+    context.args.data.date = fechaManyana;
+    next();
+  });
 };
